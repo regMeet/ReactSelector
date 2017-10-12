69af492a4c4d1af296266fb772ad176bb72ec9d6
Alan Albertengo <alan.albertengo@santexgroup.com>
2017-10-12 12:43:42 -0300

    adding debounce from lodash

--- src/components/SelectInput.js
+++ src/components/SelectInput.js
@@ -1,25 +1,38 @@
-import React, { Component, PropTypes } from 'react'
-import Select from 'react-select'
+import React, { Component, PropTypes } from 'react';
+import Select from 'react-select';
+import debounce from 'lodash.debounce';
 
 class SelectInput extends Component {
 
-  onChange(object) {
-    const {input: {onChange}} = this.props;
-    onChange(object);
+  constructor(props) {
+    super(props);
+    //this.onInputChange = debounce(this.onInputChange, 500);
   }
 
-  onInputChange(object) {
-    this.props.handleInputChange(object);
-    return object;
-  }
+  onChange = (object) => {
+    const {input: {onChange}} = this.props;
+    onChange(object);
+
+    if(!object) {
+      this.props.handleInputChange(object);
+    }
+  };
+
+  onInputChange = (inputValue) => {
+    if (inputValue && inputValue.length > 2) {
+      this.props.handleInputChange(inputValue);
+    }
+
+    return inputValue;
+  };
 
   render() {
     return (
       <Select {...this.props }
               value = { this.props.input.value || ''}
               onBlur = {() => this.props.input.onBlur(this.props.input.value) }
-              onChange = { this.onChange.bind(this) }
-              onInputChange = { this.onInputChange.bind(this) }
+              onChange = { this.onChange }
+              onInputChange = { this.onInputChange }
               options={ this.props.options}
       />
     );
